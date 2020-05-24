8f3c9439f35049fb6d2b88da18d67ff3dc7ff372
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2020-05-24 16:10:47 -0400

    add files_from_directory function

--- poctoolkit/folder_utils.py
+++ poctoolkit/folder_utils.py
@@ -1,5 +1,15 @@
 
 import os
+import glob
+
+
+def files_from_directory(src_dir, regex='*.*'):
+    """
+    return a list of all files in src_dir that match the regex
+    """
+    filename_regex = os.path.join(src_dir, regex)
+    filelist = glob.glob(filename_regex)
+    return filelist
 
 
 def absoluteFilePaths(directory: str):
