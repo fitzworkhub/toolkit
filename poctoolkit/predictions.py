4374ff83dcaade236a000da334b8db21300fb0e0
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2020-07-23 14:07:58 -0400

    write predictions out to csv file

--- poctoolkit/predictions.py
+++ poctoolkit/predictions.py
@@ -1,9 +1,21 @@
 """
 Functions to handle predictions
 """
+import csv
+import json
+import os
 
 from config import PROD_CLIENT
 from indico.queries import ModelGroupPredict, JobStatus
+
+
+def read_prediction(prediction_file):
+    """
+    Read prediction json file and return prediction list
+    """
+    with open(prediction_file) as f:
+        predictions = json.load(f)
+    return predictions
 
 
 def predict(samples, model_id, client=PROD_CLIENT):
@@ -20,10 +32,7 @@ def predict(samples, model_id, client=PROD_CLIENT):
 
     """
 
-    job = client.call(
-        ModelGroupPredict(model_id=model_id,
-                          data=samples)
-    )
+    job = client.call(ModelGroupPredict(model_id=model_id, data=samples))
 
     predictions = client.call(JobStatus(id=job.id, wait=True)).result
     return predictions
@@ -42,3 +51,40 @@ def get_top_classification(classification_preds):
         confidences.append(pred[label])
 
     return labels, confidences
+
+
+def predictions_to_csv(prediction_files, csv_filepath="predictions.csv"):
+    """
+    Given a list of prediction json data, write to csv_filepath with format
+    Identifier |  Label  |   Value  |  Confidence
+    -----------+---------+----------+-------------
+
+    If prediction_files is provided, create list of predictions by reading
+    files
+    """
+    if prediction_files:
+        prediction_lists = [
+            read_prediction(prediction_file) for prediction_file in prediction_files
+        ]
+
+    with open(csv_filepath, mode="w") as prediction_handle:
+        fieldnames = ["Filename", "Label", "Value", "Confidence"]
+        writer = csv.DictWriter(prediction_handle, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for prediction_list, prediction_filepath in zip(
+            prediction_lists, prediction_files
+        ):
+            filename = os.path.basename(prediction_filepath)
+            filename = os.path.splitext(filename)[0] + ".pdf"
+            for prediction in prediction_list:
+                value = prediction["text"]
+                label = prediction["label"]
+                confidence = prediction["confidence"][label]
+                csv_row = {
+                    "Filename": filename,
+                    "Label": label,
+                    "Value": value,
+                    "Confidence": confidence,
+                }
+                writer.writerow(csv_row)
